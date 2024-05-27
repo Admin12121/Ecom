18b27192f1fc09f8853d88644023fdd6272c45e8
Vicky <vickytaj6459@gmail.com>
2024-05-28 00:57:16 +0545

    ui and api updates-2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { SparklesCore } from "./sparkles";
-import { AcmeLogo } from "../components/Navbar/AcmeLogo";
 import FormTab from "./Form";
 import { ReactLenis, useLenis } from 'lenis/react'
 const Signup = () => {
@@ -31,52 +30,3 @@ const Signup = () => {
 };
 
 export default Signup;
-// <Card className="min-w-[400px] p-2" >
-//     <CardHeader className="flex gap-3">
-//         <AcmeLogo/>
-//         <div className="flex flex-col">
-//         <p className="text-md">Ecom</p>
-//         {/* <p className="text-small text-denfault-500">nextui.org</p> */}
-//         </div>
-//     </CardHeader>
-//     <CardBody className="gap-5">
-//         <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-//             <Input type="f_name" label="First Name" />
-//             <Input type="l_name" label="Last Name" />
-//         </div>
-//         <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-//             <Input type="email" label="Email" />
-//         </div>
-//         <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-//             <Input type="number" label="Phone" />
-//         </div>
-//         <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-//             <Input type="password" label="Password" />
-//             <Input type="password" label="Confirm Password" />
-//         </div>
-//         <div className="flex py-2 px-1 justify-between">
-//           <Checkbox
-//             classNames={{
-//               label: "text-small",
-//             }}
-//           >
-//             Accept term and conditions
-//           </Checkbox>
-//           {/* <Link color="primary" href="#" size="sm">
-//             Forgot password?
-//           </Link> */}
-//         </div>
-//     </CardBody>
-//     <CardFooter  className="flex flex-col gap-5">
-//         <Button color="secondary" className="w-full" >
-//             Sign up
-//         </Button>
-//         {/* <Divider className="my-4" />
-//         <Button color="default" className="w-full">
-//           Google
-//         </Button>
-//         <Button color="default" className="w-full">
-//           Facebook
-//         </Button> */}
-//     </CardFooter>
-// </Card>
