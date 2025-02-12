dbb0d7a7170aa58e63e7e544c910c0730bd67eb8
Admin12121 <vickytaj6459@gmail.com>
2025-02-13 00:23:31 +0545

    test 2 for appication 1

--- src/app/(app)/(user)/page.tsx
+++ src/app/(app)/(user)/page.tsx
@@ -4,15 +4,16 @@ import { readFileSync } from 'fs';
 import path from 'path';
 const LandingPage = dynamic(() => import("./_componets"));
 
-async function getSiteConfig() {
-  const filePath = path.join(process.cwd(), 'src/app/(app)/(user)/siteconfig.json');
-  const fileContents = readFileSync(filePath, 'utf8');
-  return JSON.parse(fileContents);
-}
+// async function getSiteConfig() {
+//   const filePath = path.join(process.cwd(), 'src/app/(app)/(user)/siteconfig.json');
+//   const fileContents = readFileSync(filePath, 'utf8');
+//   return JSON.parse(fileContents);
+// }
 
 
 const Page = async () => {
-  const siteConfig = await getSiteConfig();
+  const siteConfig = siteData;
+  // const siteConfig = await getSiteConfig();
   return (
     <main className="h-full max-w-[95rem] w-full">
       <LandingPage siteConfig={siteConfig}/>
@@ -21,3 +22,41 @@ const Page = async () => {
 };
 
 export default Page;
+
+
+const siteData = {
+  "slider": [
+    {
+      "image": "https://i.pinimg.com/736x/57/7f/8a/577f8ae0cc1a6c225129c86a5d9b5cdd.jpg",
+      "href": ""
+    },
+    {
+      "image": "https://i.pinimg.com/736x/24/1e/52/241e521caa67fb5ec8a2a269f9030e39.jpg",
+      "href": ""
+    },
+    {
+      "image": "https://i.pinimg.com/736x/3c/33/0f/3c330f9be98c050e9521b30d0a688d4d.jpg",
+      "href": ""
+    },
+    {
+      "image": "https://i.pinimg.com/736x/62/14/9e/62149eedf853b28b6d8802c61602984c.jpg",
+      "href": ""
+    }
+  ],
+  "events": [
+    {
+      "title": "Winter Sale",
+      "description": "30% off on all winter items",
+      "color": ""
+    },
+    {
+      "title": "Free Delivery",
+      "description": "Get free delivery on all orders",
+      "color": ""
+    }
+  ],
+  "messages": {
+    "message": "",
+    "date": ""
+  }
+}
